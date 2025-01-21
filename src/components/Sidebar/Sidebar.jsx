7ea2e4c8c3bba2b0282228b1a3879042fd3a745b
Mary Popoola <popoolamarimar@gmail.com>
2025-01-21 14:38:33 +0000

    Sidebar CSS

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
+  const [extended, setExtentded] = useState(false);
+
   return (
     <div className="sidebar">
       <div className="top">
         <img className="menu" src={assets.menu_icon} alt="" />
         <div className="new-chat">
           <img src={assets.plus_icon} alt="" />
-          <p>New Chat</p>
+          {extended ? <p>New Chat</p> : null}
         </div>
         <div className="recent">
           <p className="recent-title">Recent</p>
